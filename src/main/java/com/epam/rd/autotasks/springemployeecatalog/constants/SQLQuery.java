bdfcd04e66b3caef2cbb44fdd8b5943e59e07bd2
dzmitrytysevich <dztysevich@gmail.com>
2022-01-31 01:09:03 +0300

    some changes added to sqlQuery

--- src/main/java/com/epam/rd/autotasks/springemployeecatalog/constants/SQLQuery.java
+++ src/main/java/com/epam/rd/autotasks/springemployeecatalog/constants/SQLQuery.java
@@ -46,14 +46,15 @@ public class SQLQuery {
 
     public static final String EMPLOYEES_BY_DEP_NAME_FORMAT_QUERY =
             "WITH RECURSIVE cte (id, firstName, lastName, middleName, position, manager, hireDate, salary, department, name, location) AS ( " +
-                    "SELECT id, firstName, lastName, middleName, position, manager, hireDate, salary, department " +
+                    "SELECT e.id, firstName, lastName, middleName, position, manager, hireDate, salary, department " +
                     "FROM employee e " +
+                    "LEFT JOIN department d ON e.department = d.id " +
+                    "WHERE d.name = '%s' " +
                     "UNION ALL " +
                     "SELECT m.id, m.firstName, m.lastName, m.middleName, m.position, m.manager, m.hireDate, m.salary, m.department " +
                     "FROM employee m " +
                     "INNER JOIN cte ON cte.manager = m.id " +
                     ") SELECT DISTINCT cte.id, firstName, lastName, middleName, position, manager, hireDate as hired, salary, department, name, location FROM cte " +
                     "LEFT JOIN department d ON cte.department = d.id " +
-                    "WHERE name IS NULL OR name = '%s' " +
                     "ORDER BY %s %s";
 }
